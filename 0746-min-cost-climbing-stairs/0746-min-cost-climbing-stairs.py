e97e6d7a2484b6666ba90da13e1c5c4a21c27ec8
anshika296 <ranshika296@gmail.com>
2026-09-03 21:37:41 +0530

    Time: 3 ms (60.9%), Space: 19.2 MB (80.15%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev2=0
+        prev1=0
+        for i in range(2,len(cost)+1):
+            curr=min(prev2+cost[i-2],prev1+cost[i-1])
+            prev2=prev1
+            prev1=curr
+        return prev1 #cuz that is the updated current O(1) sc
+        
